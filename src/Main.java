bd6145120d394ad4bc5f09b39070af4d6f359a39
AlexanderRyaz <110193564+AlexanderRyaz@users.noreply.github.com>
2022-10-21 13:04:22 +0300

    2.8 задание 2.1 рецепты

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,41 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
         Food bananas = new Food("бананы", 100, 1);
         Food apples = new Food("яблоки", 50, 2);
         Food milk = new Food("молоко", 110, 1);
-        Food meat = new Food("мясо", 0, 0);
+        Food meat = new Food("мясо", 200, 1.5);
+        FoodList foodList1 = new FoodList();
         FoodList foodList = new FoodList();
-        try {
+        foodList.addFoodToList(bananas);
+        foodList.addFoodToList(apples);
+        foodList.addFoodToList(milk);
+        foodList.addFoodToList(meat);
+        foodList1.addFoodToList(bananas);
+        foodList1.addFoodToList(milk);
+        Recipe recipe = new Recipe("рецепт");
+        recipe.setFoodList(foodList);
+        Recipe recipe1 = new Recipe("рецепт 1");
+        recipe1.setFoodList(foodList1);
+        Set<Recipe> recipes = new HashSet<>();
+        Recipe recipe2 = new Recipe("рецепт 2");
+        recipe2.setFoodList(foodList1);
+        Recipe[] recipesArray = new Recipe[]{
+                recipe, recipe1, recipe2, recipe1
+        };
+        for (Recipe recipe3 : recipesArray) {
+            try {
 
 
-            foodList.addFoodToList(bananas);
-            foodList.markFood(bananas);
-            foodList.addFoodToList(apples);
-            foodList.markFood(apples);
-            foodList.addFoodToList(milk);
-            foodList.markFood(milk);
-            foodList.addFoodToList(meat);
-        } catch (RuntimeException e) {
-            e.printStackTrace();
+                if (!recipes.add(recipe3)) {
+                    throw new RuntimeException("рецепт существует");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            System.out.println("стоимость рецепта " + recipe3.getName() + " " + recipe3.getTotalCost());
         }
-        foodList.markFood(meat);
-        try {
-
-            foodList.addFoodToList(bananas);
-        } catch (RuntimeException e) {
-            e.printStackTrace();
-        }
-        foodList.markFood(bananas);
-        foodList.removeFood(apples);
-        for (Food food : foodList.getFoodList()) {
-            System.out.println(food);
-        }
-
-
     }
 }
