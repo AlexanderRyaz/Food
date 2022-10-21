77bbf6830a9d8d3eb64c31420bcfe2b35c563e49
AlexanderRyaz <110193564+AlexanderRyaz@users.noreply.github.com>
2022-10-21 12:13:40 +0300

    2.8 задание 1 список еды

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Food bananas = new Food("бананы", 100, 1);
+        Food apples = new Food("яблоки", 50, 2);
+        Food milk = new Food("молоко", 110, 1);
+        Food meat = new Food("мясо", 0, 0);
+        FoodList foodList = new FoodList();
+        try {
+
+
+            foodList.addFoodToList(bananas);
+            foodList.markFood(bananas);
+            foodList.addFoodToList(apples);
+            foodList.markFood(apples);
+            foodList.addFoodToList(milk);
+            foodList.markFood(milk);
+            foodList.addFoodToList(meat);
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        }
+        foodList.markFood(meat);
+        try {
+
+            foodList.addFoodToList(bananas);
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        }
+        foodList.markFood(bananas);
+        foodList.removeFood(apples);
+        for (Food food : foodList.getFoodList()) {
+            System.out.println(food);
+        }
+
+
     }
 }
