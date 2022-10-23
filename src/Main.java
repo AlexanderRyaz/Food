6247c464375fdc982e5a24b314b412180264e94b
AlexanderRyaz <110193564+AlexanderRyaz@users.noreply.github.com>
2022-10-23 21:31:15 +0300

    3й урок коллекции, задание 1 полностью сделано, осталось задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,62 @@
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
+
     public static void main(String[] args) {
-        Food bananas = new Food("бананы", 100, 1);
-        Food apples = new Food("яблоки", 50, 2);
-        Food milk = new Food("молоко", 110, 1);
-        Food meat = new Food("мясо", 200, 1.5);
-        FoodList foodList1 = new FoodList();
-        FoodList foodList = new FoodList();
-        foodList.addFoodToList(bananas,2);
-        foodList.addFoodToList(apples,4);
-        foodList.addFoodToList(milk,1);
-        foodList.addFoodToList(meat,8);
-        foodList1.addFoodToList(bananas,4);
-        foodList1.addFoodToList(milk,2);
-        Recipe recipe = new Recipe("рецепт");
-        recipe.setFoodList(foodList);
-        Recipe recipe1 = new Recipe("рецепт 1");
-        recipe1.setFoodList(foodList1);
-        Set<Recipe> recipes = new HashSet<>();
-        Recipe recipe2 = new Recipe("рецепт 2");
-        recipe2.setFoodList(foodList1);
-        Recipe[] recipesArray = new Recipe[]{
-                recipe, recipe1, recipe2, recipe1
-        };
-        for (Recipe recipe3 : recipesArray) {
-            try {
+        // No. 1
+        System.out.println("No. 1");
+        Map<String, List<Integer>> stringListMap = new HashMap<>();
+        for (int i = 1; i < 6; i++) {
+            String        key      = "string" + i;
+            List<Integer> integers = new ArrayList<>();
+            integers.add(ThreadLocalRandom.current().nextInt(0, 1000));
+            integers.add(ThreadLocalRandom.current().nextInt(0, 1000));
+            integers.add(ThreadLocalRandom.current().nextInt(0, 1000));
+            stringListMap.put(key, integers);
+        }
+        printMapList(stringListMap);
+        Map<String, Integer> stringIntegerMap = transformMap(stringListMap);
+        printMap(stringIntegerMap);
 
+        // No. 2
+        System.out.println("No. 2");
+        Map<Integer, String> integerStringMap = new LinkedHashMap<>();
 
-                if (!recipes.add(recipe3)) {
-                    throw new RuntimeException("рецепт существует");
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
+        int cnt = 1;
+        while (integerStringMap.size() < 10) {
+            String value = "string_" + cnt++;
+            integerStringMap.put(ThreadLocalRandom.current().nextInt(0, 100), value);
+        }
+        for (Integer key : integerStringMap.keySet()) {
+            System.out.println(key + " -> " + integerStringMap.get(key));
+        }
+
+    }
+
+    private static Map<String, Integer> transformMap(Map<String, List<Integer>> map) {
+        Map<String, Integer> newMap = new HashMap<>();
+        for (String s : map.keySet()) {
+            int sum = 0;
+            for (Integer integer : map.get(s)) {
+                sum += integer;
             }
-            System.out.println("стоимость рецепта " + recipe3.getName() + " " + recipe3.getTotalCost());
+            newMap.put(s, sum);
+        }
+
+        return newMap;
+    }
+
+    private static void printMap(Map<String, Integer> map) {
+        for (String key : map.keySet()) {
+            System.out.println(key + " -> " + map.get(key));
         }
     }
+
+    private static void printMapList(Map<String, List<Integer>> map) {
+        for (String key : map.keySet()) {
+            System.out.println(key + " -> [" + Arrays.toString(map.get(key).toArray()) + "]");
+        }
+    }
+
 }
